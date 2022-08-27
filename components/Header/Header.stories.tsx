284d8e1913b8b39cefe266b8dd86711a02a8ece8
Rabii Luena <itisrabii@gmail.com>
2022-08-27 17:26:37 +0300

    feat: adding dummy header

--- components/Header/Header.stories.tsx
+++ components/Header/Header.stories.tsx
@@ -1,1 +1,12 @@
-import Header from './Header.stories';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Header from './Header';
+
+export default {
+  title: 'Layout/Header',
+  component: Header,
+  argTypes: {},
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const L = Template.bind({});
